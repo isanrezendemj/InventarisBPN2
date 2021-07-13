11c611a370e95620d8663c14322ac98358445531
isanrezendemj <lisanmoe2018@gmail.com>
2021-07-13 17:07:11 +0700

    tes

--- resources/views/editpenggunabidang.blade.php
+++ resources/views/editpenggunabidang.blade.php
@@ -51,7 +51,6 @@
                 </div>
             </div>
 
-
             <div class="form-group row">
                 <label for="Kode Bidang" class="col-2">Kode Bidang:</label>
                 <div class="col-10">
