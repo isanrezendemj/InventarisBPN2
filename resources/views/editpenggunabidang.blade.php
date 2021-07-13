ddfdacd0cd1dba4c2fddb5455d40bb51e72c25d9
isanrezendemj <lisanmoe2018@gmail.com>
2021-07-13 17:04:42 +0700

    tes

--- resources/views/editpenggunabidang.blade.php
+++ resources/views/editpenggunabidang.blade.php
@@ -12,6 +12,7 @@
     <div class="padding_this">
         <form method="POST" action="{{url('/proses-edit-penggunabidang')}}" enctype="multipart/form-data">
             {{csrf_field()}}
+
             <div class="form-group row">
                 <label for="Kode Bidang" class="col-2">Kode Bidang:</label>
                 <div class="col-10">
@@ -47,79 +48,62 @@
                     <script>
                     $("#Nama_Bidang").val("{{$article->Nama_Bidang}}");
                     </script>
-
-                    <div class="container">
-                        <div class="padding_this">
-                            <form method="POST" action="{{url('/proses-edit-penggunabidang')}}"
-                                enctype="multipart/form-data">
-                                {{csrf_field()}}
-
-                                <div class="form-group row">
-                                    <label for="Kode Bidang" class="col-2">Kode Bidang:</label>
-                                    <div class="col-10">
-                                        <input type="text" value="{{$article->Kode_Bidang}}"
-                                            onkeypress="return angka(event)" class="form-control" readonly="true"
-                                            name="Kode_Bidang" value="{{$article->id}}" placeholder="Kode bidang"
-                                            required oninvalid="this.setCustomValidity('harap isikan ')"
-                                            oninput="this.setCustomValidity('')" autocomplete="off">
-                                    </div>
-
-                                </div>
-                        </div>
+                </div>
+            </div>
 
 
-                        <div class="form-group row">
-                            <label for="No_Aset" class="col-2">No Aset:</label>
-                            <div class="col-10">
-                                <input type="text" value="{{$article->No_Aset}}" class="form-control" name="No_Aset"
-                                    placeholder="No Aset" required
-                                    oninvalid="this.setCustomValidity('harap isikan dengan lengkap')"
-                                    oninput="this.setCustomValidity('')" autocapitalize="off">
-                            </div>
-                        </div>
+            <div class="form-group row">
+                <label for="Kode Bidang" class="col-2">Kode Bidang:</label>
+                <div class="col-10">
+                    <input type="text" value="{{$article->Kode_Bidang}}" onkeypress="return angka(event)"
+                        class="form-control" readonly="true" name="Kode_Bidang" value="{{$article->id}}"
+                        placeholder="Kode bidang" required oninvalid="this.setCustomValidity('harap isikan ')"
+                        oninput="this.setCustomValidity('')" autocomplete="off">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="No_Aset" class="col-2">No Aset:</label>
+                <div class="col-10">
+                    <input type="text" value="{{$article->No_Aset}}" class="form-control" name="No_Aset"
+                        placeholder="No Aset" required oninvalid="this.setCustomValidity('harap isikan dengan lengkap')"
+                        oninput="this.setCustomValidity('')" autocapitalize="off">
+                </div>
+            </div>
 
-                        <div class="form-group row">
-                            <label for="Nama Barang" class="col-2">Nama Barang:</label>
-                            <div class="col-10">
-                                <input type="text" value="{{$article->Nama_Barang}}" class="form-control"
-                                    name="Nama_Barang" placeholder="Nama Barang" required
-                                    oninvalid="this.setCustomValidity('harap isikan dengan lengkap')"
-                                    oninput="this.setCustomValidity('')" autocapitalize="off">
-                            </div>
-                        </div>
+            <div class="form-group row">
+                <label for="Nama Barang" class="col-2">Nama Barang:</label>
+                <div class="col-10">
+                    <input type="text" value="{{$article->Nama_Barang}}" class="form-control" name="Nama_Barang"
+                        placeholder="Nama Barang" required
+                        oninvalid="this.setCustomValidity('harap isikan dengan lengkap')"
+                        oninput="this.setCustomValidity('')" autocapitalize="off">
+                </div>
+            </div>
 
+            <div class="form-group row">
+                <label for="Kondisi" class="col-2">Kondisi:</label>
+                <div class="col-10">
+                    <select type="text" value="{{$article->Kondisi}}" name="Kondisi" id="kondisi" class="form-control">
+                        <option value="">Kondisi Barang</option>
+                        <option value="bagus">Bagus</option>
+                        <option value="rusak">Rusak</option>
 
+                    </select>
+                    <script>
+                    $("#kondisi").val("{{$article->Kondisi}}");
+                    </script>
+                </div>
+            </div>
 
-                        <div class="form-group row">
-                            <label for="Kondisi" class="col-2">Kondisi:</label>
-                            <div class="col-10">
-                                <select type="text" value="{{$article->Kondisi}}" name="Kondisi" id="kondisi"
-                                    class="form-control">
-                                    <option value="">Kondisi Barang</option>
-                                    <option value="bagus">Bagus</option>
-                                    <option value="rusak">Rusak</option>
+            <div class="form-group row">
+                <label for="Image" class="col-2">Image :</label>
+                <div class="col-10">
+                    <input type="file" name="Image" placeholder="Ambiol" required>
+                </div>
+            </div>
 
-                                </select>
-                                <script>
-                                $("#kondisi").val("{{$article->Kondisi}}");
-                                </script>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="Image" class="col-2">Image :</label>
-                            <div class="col-10">
-                                <input type="file" name="Image" placeholder="Ambiol" required>
-                            </div>
-                        </div>
-
-                        <button type="submit" style="background-color:#452414; color:white;">
-                            Simpan</button>
+            <button type="submit" style="background-color:#452414; color:white;">
+                Simpan</button>
         </form>
     </div>
     @endsection
-
-    <button type="submit" style="background-color:#452414; color:white;">Simpan</button>
-    </form>
-</div>
-@endsection
